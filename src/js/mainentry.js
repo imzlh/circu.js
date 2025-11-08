39516b45546d3b2bbaa63f36fa4036679a7733bc
imzlh <2131601562@qq.com>
2025-11-09 07:37:49 +0800

    remove js polyfills

--- src/js/mainentry.js
+++ src/js/mainentry.js
@@ -1,2 +1,22 @@
-const { stdout } = import.meta.use('process')
-throw new Error("No main entry point found. Please bundle your application into this binary first.");
+/**
+ * txiki.js test suite entry point.
+ */
+
+const { use } = import.meta;
+globalThis.use = use;
+
+const self = use("sys");
+const os = use("os");
+if(self.args.length < 2){
+    print(`Usage: tjs <script.js>
+Run a txiki.js test suite script.`);
+    os.exit(1);
+}
+
+const [, script] = self.args
+if(!script || !script.endsWith(".js")){
+    throw new Error("Script must be a .js file");
+}
+
+print("Test suite, tjs", self.version, "on", self.platform);
+await self.evalFile(script);
--- src/js/polyfills/timers.js
+++
@@ -1,7 +0,0 @@
-const core = globalThis[Symbol.for('tjs.internal.core')];
-
-globalThis.setTimeout = core.setTimeout;
-globalThis.clearTimeout = core.clearTimeout;
-
-globalThis.setInterval = core.setInterval;
-globalThis.clearInterval = core.clearInterval;
